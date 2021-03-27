6a5e9439002de361d9618a9a819db49fddf8ac68
Abdullah-AlSawalmeh <abdullahalsawalmeh@gmail.com>
2021-03-28 00:20:46 +0300

    Finish the project

--- js/app.js
+++ js/app.js
@@ -13,30 +13,23 @@ function Submit(name, age, note) {
   this.age = age;
   this.note = note;
   //   submits_array.push(this);
-    LSsubmitsLS.push(this);
+  LSsubmitsLS.push(this);
 
 
 
 }
 
 Submit.prototype.render = function () {
-    let name = document.createElement('p');
-    let age = document.createElement('p');
-    let note = document.createElement('p');
-    name.textContent = this.name;
-    age.textContent = this.age;
-    note.textContent = this.note;
-    div.appendChild(name);
-    div.appendChild(age);
-    div.appendChild(note);
-}
-
-
-
-
-
-
-
+  let name = document.createElement('p');
+  let age = document.createElement('p');
+  let note = document.createElement('p');
+  name.textContent = this.name;
+  age.textContent = this.age;
+  note.textContent = this.note;
+  div.appendChild(name);
+  div.appendChild(age);
+  div.appendChild(note);
+};
 
 
 
@@ -45,8 +38,8 @@ document.addEventListener('submit', SHandler);
 
 function SHandler(event) {
   event.preventDefault();
-   let x = new Submit(event.target.namen.value, event.target.agen.value, event.target.noten.value);
-    x.render();
+  let x = new Submit(event.target.namen.value, event.target.agen.value, event.target.noten.value);
+  x.render();
   // console.log(submits_array);
   localStorage.setItem('submits', JSON.stringify(LSsubmitsLS));
 }
