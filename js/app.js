7a272a48681ec6a8b1f10cabf1ef2f1776924d7b
Abdullah-AlSawalmeh <abdullahalsawalmeh@gmail.com>
2021-03-27 23:49:40 +0300

    Finish the project

--- js/app.js
+++ js/app.js
@@ -1,8 +1,8 @@
 // let submits_array = [];
-
+let div = document.getElementById('div');
 if (localStorage.getItem('submits') === null) {
 
-    localStorage.setItem('submits', JSON.stringify([]));
+  localStorage.setItem('submits', JSON.stringify([]));
 }
 
 let LSsubmitsLS = JSON.parse(localStorage.getItem('submits'));
@@ -12,20 +12,24 @@ function Submit(name, age, note) {
   this.name = name;
   this.age = age;
   this.note = note;
-//   submits_array.push(this);
+  //   submits_array.push(this);
     LSsubmitsLS.push(this);
 
-    
-  }
 
 
+}
 
-
-
-
-
-
-
+Submit.prototype.render = function () {
+    let name = document.createElement('p');
+    let age = document.createElement('p');
+    let note = document.createElement('p');
+    name.textContent = this.name;
+    age.textContent = this.age;
+    note.textContent = this.note;
+    div.appendChild(name);
+    div.appendChild(age);
+    div.appendChild(note);
+}
 
 
 
@@ -41,7 +45,8 @@ document.addEventListener('submit', SHandler);
 
 function SHandler(event) {
   event.preventDefault();
-    new Submit(event.target.namen.value, event.target.agen.value, event.target.noten.value);
+   let x = new Submit(event.target.namen.value, event.target.agen.value, event.target.noten.value);
+    x.render();
   // console.log(submits_array);
-    localStorage.setItem('submits', JSON.stringify(LSsubmitsLS));
+  localStorage.setItem('submits', JSON.stringify(LSsubmitsLS));
 }
--- js/notes.js
+++ js/notes.js
@@ -5,19 +5,19 @@ console.log(LSsubmits);
 let body = document.querySelector('body');
 
 for (let i = 0; i < LSsubmits.length; i++) {
-    let div = document.createElement('div');
-    div.className = 'myDiv';
+  let div = document.createElement('div');
+  div.className = 'myDiv';
   let name = document.createElement('p');
   let age = document.createElement('p');
-    let note = document.createElement('p');
-    // let br = document.createElement('br');
+  let note = document.createElement('p');
+  // let br = document.createElement('br');
   name.textContent = LSsubmits[i].name;
   age.textContent = LSsubmits[i].age;
   note.textContent = LSsubmits[i].note;
   div.appendChild(name);
   div.appendChild(age);
-    div.appendChild(note);
-    body.appendChild(div)
-   
+  div.appendChild(note);
+  body.appendChild(div);
+
 
 }
